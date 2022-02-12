dcc72cf9d263f60ba652e7923ab6e70fa773db95
migmatore <kazakvova201@gmail.com>
2022-02-12 21:27:31 +0300

    Added ipv4 header parsing

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,33 @@ fn main() -> io::Result<()>  {
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
 
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
 
         // if no ipv4 packet
-        if proto != 0x0800 {
+        if eth_proto != 0x0800 {
             continue
         }
 
-        eprintln!(
-            "read {} bytes (flags: {:x}, proto: {:x}): {:x?}", 
-            nbytes - 4,
-            flags,
-            proto, 
-            &buf[..nbytes]
-        );
-    }
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
 
-    Ok(())
+                eprintln!("{} → {} {}b of protocol {}", src, dst, proto, p.payload_len());
+            },
+            Err(e) => {
+                eprintln!("ignoring weird packet {:?}", e);
+            }
+        }
+
+        // eprintln!(
+        //     "read {} bytes (flags: {:x}, proto: {:x}): {:x?}", 
+        //     nbytes - 4,
+        //     flags,
+        //     proto, 
+        //     &buf[..nbytes]
+        // );
+    }
 }
