a3bb7c8589175524e285e2b11c2c728dfe11115f
migmatore <kazakvova201@gmail.com>
2022-02-12 15:36:17 +0300

    Update run.sh script. Read 4 bytes from header.

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 cargo build
 sudo cap cap_net_admin=eip target/debug/thunder
 target/debug/thunder &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ fn main() -> io::Result<()>  {
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
 
-        eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+
+        eprintln!("read {} bytes: {:x?}", nbytes - 4, &buf[..nbytes]);
     }
 
     Ok(())
