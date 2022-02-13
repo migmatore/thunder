85986e8caa603391118bc3f4327efd64c1b39f9a
migmatore <kazakvova201@gmail.com>
2022-02-13 21:14:49 +0300

    Update run.sh and main.rs Added tcp header parsing

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 cargo build
 sudo setcap cap_net_admin=eip target/debug/thunder
+sudo chmod 666 /sys/class/leds/sys_led/trigger
+sudo chmod 666 /sys/class/leds/red_red/trigger
+echo none > /sys/class/leds/sys_led/trigger
+echo heartbeat > /sys/class/leds/red_red/trigger
 target/debug/thunder &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
-trap "kill $pid" INT TERM
+trap "kill $pid; echo none > /sys/class/leds/red_red/trigger" INT TERM
 wait $pid
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use tun_tap::{Iface, Mode};
 use std::io;
+use std::fs;
+use std::{thread, time::Duration};
 
 fn main() -> io::Result<()>  {
     let nic = Iface::new("tun0", Mode::Tun)?;
+    //fs::write("/sys/class/leds/red_red/trigger", "heartbeat").expect("error");
 
     let mut buf = [0u8; 1504];
 
@@ -12,24 +15,45 @@ fn main() -> io::Result<()>  {
         let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
         let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
 
-        // if no ipv4 packet
+        // if not ipv4 packet
         if eth_proto != 0x0800 {
-            continue
+            continue;
         }
 
+        // parsing ipv4 header
         match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
             Ok(p) => {
                 let src = p.source_addr();
                 let dst = p.destination_addr();
                 let proto = p.protocol();
 
-                eprintln!("{} → {} {}b of protocol {}", src, dst, proto, p.payload_len());
+                // if not tcp packet
+                if proto != 0x06 {
+                    continue;
+                }
+                
+                // parsing tcp header
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + p.slice().len()..]) {
+                    Ok(p) => {
+                        // (srcip, srcport, dstip, dstport) = 
+                        eprintln!(
+                            "{} → {} {}b of tcp to port {}", 
+                            src, 
+                            dst, 
+                            p.slice().len(),
+                            p.destination_port()
+                        );
+                    },
+                    Err(e) => {
+                        eprintln!("ignoring weird tcp packet {:?}", e);
+                    }
+                }
             },
             Err(e) => {
-                eprintln!("ignoring weird packet {:?}", e);
+                eprintln!("ignoring weird ip packet {:?}", e);
             }
         }
-
+        
         // eprintln!(
         //     "read {} bytes (flags: {:x}, proto: {:x}): {:x?}", 
         //     nbytes - 4,
