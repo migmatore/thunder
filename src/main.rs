6cef2b324a5851ac0f65eff041a58ff6ac1ed0b2
migmatore <kazakvova201@gmail.com>
2022-02-21 23:06:53 +0300

    Get only packet data without info

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct Quad {
 fn main() -> io::Result<()> {
     let mut connections: HashMap<Quad, tcp::Connection> = Default::default();
 
-    let mut nic = Iface::new("tun0", Mode::Tun)?;
+    let mut nic = Iface::without_packet_info("tun0", Mode::Tun)?;
     //fs::write("/sys/class/leds/red_red/trigger", "heartbeat").expect("error");
 
     let mut buf = [0u8; 1504];
@@ -23,16 +23,16 @@ fn main() -> io::Result<()> {
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
 
-        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        // let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        // let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
 
-        // if not ipv4 packet
-        if eth_proto != 0x0800 {
-            continue;
-        }
+        // // if not ipv4 packet
+        // if eth_proto != 0x0800 {
+        //     continue;
+        // }
 
         // parsing ipv4 header
-        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[..nbytes]) {
             Ok(ip_header) => {
                 let src = ip_header.source_addr();
                 let dst = ip_header.destination_addr();
@@ -44,12 +44,12 @@ fn main() -> io::Result<()> {
 
                 // parsing tcp header
                 match etherparse::TcpHeaderSlice::from_slice(
-                    &buf[4 + ip_header.slice().len()..nbytes],
+                    &buf[ip_header.slice().len()..nbytes],
                 ) {
                     Ok(tcp_header) => {
                         use std::collections::hash_map::Entry;
 
-                        let datai = 4 + ip_header.slice().len() + tcp_header.slice().len();
+                        let datai = ip_header.slice().len() + tcp_header.slice().len();
 
                         match connections.entry(Quad {
                             src: (src, tcp_header.source_port()),
