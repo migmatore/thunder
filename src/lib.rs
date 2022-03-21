8f20b6d1bceca9927c8a60930b533ed62af6144e
migmatore <kazakvova201@gmail.com>
2022-03-21 06:36:15 +0000

    Update Interface impl in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,49 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::prelude::*;
 
-pub struct Interface {}
+pub struct Interface {
+    connections: HashMap<Quad, tcp::Connection>,
+    nic: tun_tap::Iface,
+    buf: [u8; 1504],
+}
 
-impl Default for Interface {
-    fn default() -> Self {
-        let mut connections: HashMap<Quad, tcp::Connection> = Default::default();
-        
-        let mut nic = Iface::without_packet_info("tun0", Mode::Tun)?;
+impl Interface {
+    pub fn new() -> io::Result<Self> {
+        Ok(Interface {
+            connections: Default::default(),
+            nic: tun_tap::Iface::without_packet_info("tun0", tun_tap::Mode::Tun)?,
+            buf: [0u8; 1504],
+        })
+    }
 
-        let mut buf = [0u8; 1504];
-
-        Interface {
-            connections, nic, buf
-        }
+    pub fn bind(&mut self, port: u16) -> io::Result<TcpListener> {
+        unimplemented!()
     }
 }
 
 pub struct TcpStream {}
 
 impl Read for TcpStream {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {}
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        unimplemented!()
+    }
 }
 
 impl Write for TcpStream {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {}
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        unimplemented!()
+    }
 
-    fn flush(&mut self) -> io::Result<()> {}
+    fn flush(&mut self) -> io::Result<()> {
+        unimplemented!()
+    }
 }
 
 pub struct TcpListener {}
 
 impl TcpListener {
-    pub fn accept(&mut self) -> io::Result<TcpStream> {}
+    pub fn accept(&mut self) -> io::Result<TcpStream> {
+        unimplemented!()
+    }
 }
